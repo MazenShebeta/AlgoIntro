62ab3fa5224c8608d03dd7c8f4402597146b81c1
MazenShebeta <mazenshebeta@gmail.com>
2022-08-06 16:51:27 +0200

    added solutions of the two problems

---
+++ First Task.js
@@ -0,0 +1,20 @@
+    var word_counter = 1;
+    var vowel_counter = 0;
+    var str = "Hello World.";
+
+
+
+    str.toLowerCase;
+    for (var i = 0; i < str.length-1; i++) {
+        if (str[i] == " ") {
+            word_counter++;
+        }
+        else if (str[i] == "a" || str[i] == "e" || str[i] == "i" || str[i] == "o" || str[i] == "u" || str[i] == "y") {
+            vowel_counter++;
+        }
+        else if (str[i] == ".") {
+            break;
+        }
+    }
+    console.log("Number of words: " + word_counter);
+    console.log("Number of vowels: " + vowel_counter);
---
+++ Second Task.js
@@ -0,0 +1,34 @@
+// Given two sets of elements, find the sum of all distinct elements from the set. In other words, find the sum of all elements which are present in either of the given set.
+// Example:
+// Set 1 : [3, 1, 7, 9], Set 2: [2, 4, 1, 9, 3]
+// Output: 13 (distinct elements 4, 7, 2 )
+// Use an array as a method.
+// Initialize sum = 0. Compare each element of set one with the second set and if element is not present then add that element to sum. Then do the vice versa to add elements from the second set.
+
+var set1 = [3, 1, 7, 9];
+var set2 = [2, 4, 1, 9, 3];
+var sum = 0;
+
+for (var i = 0; i < set1.length; i++) {
+    for (var j = 0; j < set2.length; j++) {
+        if (set1[i] == set2[j]) {
+            break;
+        }
+        else if (j == set2.length-1) {
+            sum += set1[i];
+        }
+    }
+}
+
+for (var i = 0; i < set2.length; i++) {
+    for (var j = 0; j < set1.length; j++) {
+        if (set2[i] == set1[j]) {
+            break;
+        }
+        else if (j == set1.length-1) {
+            sum += set2[i];
+        }
+    }
+}
+
+console.log(sum);
